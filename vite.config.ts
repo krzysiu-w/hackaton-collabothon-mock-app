3cc60ed40f80c76920cf8fe6e4517a7dae7ac6f8
MAKUSref <tomaszmakowski002731@gmail.com>
2023-10-22 11:08:21 +0200

    manifest

--- vite.config.ts
+++ vite.config.ts
@@ -4,39 +4,22 @@ import react from "@vitejs/plugin-react";
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'prompt',
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
   manifest: {
-    name: 'Carbbyn',
-    short_name: 'Carbbyn',
-    description: 'Your carbon credists in your pocket',
+    theme_color: "#1D0093",
+    background_color: "#1D0093",
+    name: 'TreePlanter',
+    short_name: 'TreePlanter',
+    description: 'Plant trees',
+    orientation:'portrait',
+    scope: "/",
+    start_url: "/",
     icons: [
       {
-        src: "/logo192.png",
-        sizes: "192x192",
-        type: "image/png"
-      },
-      {
-        src: "/logo512.png",
+        src: "/ico.png",
         sizes: "512x512",
         type: "image/png"
-      },
-      {
-        src: '/logo192.png',
-        sizes:'192x192',
-        type:'image/png',
-        purpose:'apple touch icon',
-      },
-      {
-        src: '/logo512.png',
-        sizes:'512x512',
-        type:'image/png',
-        purpose:'any maskable',
       }
-    ],
-    display:"standalone",
-    scope:'/',
-    start_url:"/",
-    orientation:'portrait'
+    ]
   }
 }
 
